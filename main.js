cba9973857e6ebe013c401e8074fd6a1ef6ec9e6
Udara Lakshan <ulakshan061@gmail.com>
2024-10-17 13:57:23 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import { HfInference } from "@huggingface/inference"
+
+const hf = new HfInference(secrets.API_KEY)
+
+const prompt = document.getElementById("prompt")
+const button = document.getElementById("button")
+const image = document.getElementById("image")
+
+button.addEventListener("click", generateImage)
+
+async function generateImage() {
+
+	image.src = "./images/loader.svg"
+	image.alt = "Loading..."
+
+	const response = await hf.textToImage({
+		model: "black-forest-labs/FLUX.1-dev",
+		inputs: prompt.value
+	})
+
+	image.src = response ? URL.createObjectURL(response) : "./images/loader.svg"
+	image.alt = prompt.value
+
+	return image
+}
